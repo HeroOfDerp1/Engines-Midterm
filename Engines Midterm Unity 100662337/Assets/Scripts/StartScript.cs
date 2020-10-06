2b1415e5bae0f3b6119c2132e69784e65399bb99
Kaylyn <kaylyn.mccue@ontariotechu.net>
2020-10-06 18:53:06 -0400

    The Buttons Work

--- Engines Midterm Unity 100662337/Assets/Scripts/StartScript.cs
+++ Engines Midterm Unity 100662337/Assets/Scripts/StartScript.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 //The purpose of this script is to add functionality to the buttons on the start scene
 //the buttons will transition to the play or end scene, depending on the button pressed
 
-public class NewBehaviourScript : MonoBehaviour
+public class StartScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -24,12 +24,18 @@ public class NewBehaviourScript : MonoBehaviour
     //function to allow the play button to switch between the start scene and the play scene
     public void PlayTransition()
     {
+        //load the scene
         SceneManager.LoadScene("Play");
+        //debug line
+        //Debug.Log("We loaded the play scene");
     }
 
     //function to allow the stats button to switch between the start scene and the end scene
     public void StatsTransition()
     {
+        //load the scene
         SceneManager.LoadScene("End");
+        //debug line
+        //Debug.Log("We loaded the end scene");
     }
 }
