753523e8282a325a7498820e2b0372862f2e790a
Kaylyn <kaylyn.mccue@ontariotechu.net>
2020-10-08 14:54:51 -0400

    Implemented checkpoints and respawning

---
+++ Engines Midterm Unity 100662337/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Kaylyn McCune - 100662337
+//Checks for checkpoint collision and passes that checkpoint to the maze overlord
+
+public class Checkpoint : MonoBehaviour
+{
+    //create a maze overlord to pass the checkpoint to
+    public MazeOverlord mazeOverlord;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //check for collision and passes the checkpoint in as the current active checkpoint
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "player")
+        {
+            mazeOverlord.currentCheckpoint = gameObject;
+        }
+    }
+}
--- Engines Midterm Unity 100662337/Assets/Scripts/MazeOverlord.cs
+++ Engines Midterm Unity 100662337/Assets/Scripts/MazeOverlord.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 //Kaylyn McCune - 100662337
 //The script that controls global aspects of the maze including:
 //-death plane
+//-respawn and checkpoints
 
 public class MazeOverlord : MonoBehaviour
 {
     //create game object variable for player
     public GameObject player;
+    //create a game object for the checkpoints
+    public GameObject currentCheckpoint;
 
     // Start is called before the first frame update
     void Start()
@@ -25,9 +28,9 @@ public class MazeOverlord : MonoBehaviour
         //compares the current y position to that of the death plane to see if the player has fallen
         if (temp.y <= -3)
         {
-            //the player has fallen, respawn them
-            temp = new Vector3(15.7f, 1.64f, 23.13f);
-            player.transform.position = temp;
+            //the player has fallen, respawn them at the current checkpoint
+            player.transform.position = currentCheckpoint.transform.position;
         }
+
     }
 }
