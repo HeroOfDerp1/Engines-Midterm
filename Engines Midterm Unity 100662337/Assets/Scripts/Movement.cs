303317274b565f43b308e35aa62a9e7ab421ff55
Kaylyn <kaylyn.mccue@ontariotechu.net>
2020-10-07 21:33:54 -0400

    Attached Camera to Player stand in

--- Engines Midterm Unity 100662337/Assets/Scripts/Movement.cs
+++ Engines Midterm Unity 100662337/Assets/Scripts/Movement.cs
@@ -4,10 +4,14 @@ using System.Collections.Specialized;
 using System.Security.Cryptography;
 using UnityEngine;
 
+//Kaylyn McCune - 100662337
+//controls player movement
+
 public class Movement : MonoBehaviour
 {
     //declare variables
     float speed, rotSpeed;
+    public Quaternion initialRot;
 
     // Start is called before the first frame update
     void Start()
@@ -15,20 +19,21 @@ public class Movement : MonoBehaviour
         //initialize variables
         speed = 0.01f;
         rotSpeed = 0.3f;
+        initialRot = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
         //the secret jump mechanic that can never see the light of day
-        //if (Input.GetKeyDown(KeyCode.S))
+        //if (Input.GetKeyDown(KeyCode.Space))
         //{
         //    Vector3 position = transform.position;
         //    transform.position = position + new Vector3(0.0f, 1.0f, 0.0f);
         //}
 
         //movement
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             transform.position = transform.position + (transform.forward * speed);
         }
@@ -43,6 +48,11 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f) * rotSpeed, Space.World);
+        }
+        //resets the blocks rotation incase he rotates and you cant move properly
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.rotation = initialRot;
         }
     }
 }
